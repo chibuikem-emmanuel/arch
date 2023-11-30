d5821a4a9c33179cb735e6bd283deaebfbf19643
chizaram <chizaramchibuikem@gmail.com>
2023-11-30 02:45:15 -0800

    made changes to allowed_hosts in settings.py

--- sproject/settings.py
+++ sproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_8bt*+9xwcs8j*q8m2ee^ntvmyj!_@5xkp57+ey!#!g+aluvh@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://architet.onrender.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
